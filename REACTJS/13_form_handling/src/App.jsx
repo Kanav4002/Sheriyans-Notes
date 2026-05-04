3dec54402d3ac9d9e7cf40372246609dab72f42d
Kanav Kumar <kumarkanav5753@gmail.com>
2026-05-04 21:06:48 +0530

    basic form handling done

---
+++ REACTJS/13_form_handling/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+
+  const submitHandler = () => {
+    console.log("Form submitted");
+  } 
+
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        submitHandler();
+      }}>
+        <input type="text" placeholder='Enter your name:' />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
